814d66d1cdcfc02a20c08e87f193d084ab18246c
tenaxz <tenaxz@192.168.1.11>
2026-05-31 10:55:40 -0500

    first deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+
+site: 'https://unsol-pe.top',
+
+integrations: [
+
+sitemap()
+
+]
+
+});

40f5e0da161bcd815dab32ea0dacda553599b814
tenaxz <tenaxz@192.168.1.11>
2026-06-07 14:26:36 -0500

    fix redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
 
-site:"https://unsol-pe.top",
+site:'https://unsol-pe.top',
 
-integrations:[
+trailingSlash:'always',
 
-sitemap()
-
-]
+integrations:[sitemap()]
 
 });

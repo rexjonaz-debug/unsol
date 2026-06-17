2b0f4e020b061562b1ec1c905bde92335660c801
tenaxz <tenaxz@192.168.1.11>
2026-06-17 07:17:16 -0500

    Create gaming portal

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://unsol-pe.top',
 
-site:'https://unsol-pe.top',
+  trailingSlash: 'always',
 
-trailingSlash:'always',
-
-integrations:[sitemap()]
-
+  integrations: [
+    sitemap()
+  ]
 });
